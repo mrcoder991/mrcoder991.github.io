d278d110a7f7e0f4929cfb80b26e430f16ad6303
mrcoder991 <udaygirhepunje41@gmail.com>
2022-07-26 23:42:07 +0530

    profile image and animated text

--- script.js
+++ script.js
@@ -1,52 +1,25 @@
 const hamburger = document.querySelector(".hamburger");
-        const links = document.querySelector(".links");
+const links = document.querySelector(".links");
 
-        hamburger.addEventListener("click", mobileMenu);
+console.log(hamburger)
 
-        function mobileMenu() {
-            hamburger.classList.toggle("active");
-            links.classList.toggle("active");
-        }
+hamburger.addEventListener("click", mobileMenu);
 
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    links.classList.toggle("active");
+}
 
 
 function closeNav() {
     document.getElementById("hamburger").classList.remove("active");
     document.getElementById("links").classList.remove("active");
 }
-        
-
-// const translate = document.querySelectorAll(".translate");
-// const big_title = document.querySelector(".big-title");
-// const header = document.querySelector("header");
-// const shadow = document.querySelector(".shadow");
-// const content = document.querySelector(".content");
-// const section = document.querySelector("section");
-// const image_container = document.querySelector(".imgContainer");
-// const opacity = document.querySelectorAll(".opacity");
-// const border = document.querySelector(".border");
-
-// let header_height = header.offsetHeight;
-// let section_height = section.offsetHeight;
-
-// window.addEventListener('scroll', () => {
-//     let scroll = window.pageYOffset;
-//     let sectionY = section.getBoundingClientRect();
-    
-//     translate.forEach(element => {
-//         let speed = element.dataset.speed;
-//         element.style.transform = `translateY(${scroll * speed}px)`;
-//     });
-
-//     opacity.forEach(element => {
-//         element.style.opacity = scroll / (sectionY.top + section_height);
-//     })
-
-//     big_title.style.opacity = - scroll / (header_height / 2) + 1;
-//     shadow.style.height = `${scroll * 0.5 + 300}px`;
-
-//     content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
-//     image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
-
-//     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
-// })
+   
+var typed = new Typed(".typing-text", {
+    strings: ["Web Developer", "Frontend Developer", "Graphics Designer"],
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 20,
+    backDelay: 600,
+  });
