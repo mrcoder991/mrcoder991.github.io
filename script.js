9e1395dce940b230cec917ac1580c225ac51aefe
mrcoder991 <udaygirhepunje41@gmail.com>
2022-09-12 10:12:20 +0530

    Updates

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('from script')
+
+var typed = new Typed(".typing-text", {
+    strings: ["Web Developer", "Frontend Developer", "Graphics Designer"],
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 20,
+    backDelay: 600,
+});
+  
